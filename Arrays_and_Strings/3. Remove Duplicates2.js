64fe912b6a551884d640507c101d4e0ba5c7ece7
carshy <mussocollins@gmail.com>
2024-10-10 04:56:36 +0300

    Add correct practice

--- Arrays_and_Strings/3. Remove Duplicates2.js
+++ Arrays_and_Strings/3. Remove Duplicates2.js
@@ -22,14 +22,13 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 */
 
 const removeDuplicates2 = (nums) => {
-  let results = [];
+  let k = 0;
   for(let i = 0; i < nums.length; i++) {
     if(nums[i] !== 2 * (nums[i - 1])) {
-      results.push(nums[i]);
-    //   nums[k] = nums[i];
-    //   k++;
+      nums[k] = nums[i];
+      k++;
     }
   }
-  return results;
+  return k;
 }
 console.log(removeDuplicates2([1,1,1,2,2,3])) // Expected Output 5 [1, 1, 2, 2, 3]
