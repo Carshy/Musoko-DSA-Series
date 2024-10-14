e68bb931294fe3f164318d5508f907bb930e9413
carshy <mussocollins@gmail.com>
2024-10-14 20:20:28 +0300

    Add valid palindrome

---
+++ Two_Pointers/1. Valid Palindrome.js
@@ -0,0 +1,25 @@
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+*/
+
+const validPalindrome = (s) => {
+  let strLower = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  
+  return strLower;
+
+};
+console.log(validPalindrome("A man, a plan, a canal: Panama")); //true
+console.log(validPalindrome("race a car")); //false
