4c692ad211a74437efa05c80286dda7c3844aef2
carshy <mussocollins@gmail.com>
2024-10-21 15:59:03 +0300

    Use a different approach

--- Arrays_and_Vectors/1. Two Sum.js
+++ Arrays_and_Vectors/1. Two Sum.js
@@ -24,7 +24,7 @@ const twoSum = (nums, target) => {
   for(let i = 0; i < nums.length; i++) {
     let diff = target - nums[i];
     if(map[diff] !== undefined) {
-      return [map[diff], i]
+      return [diff, nums[i]]
     }
     map[nums[i]] = i;
   }
