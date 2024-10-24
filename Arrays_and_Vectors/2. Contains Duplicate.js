fe5dce093734862ab668f821cedbf52dae976ecc
carshy <mussocollins@gmail.com>
2024-10-24 16:30:46 +0300

    Add duplicate challenge

---
+++ Arrays_and_Vectors/2. Contains Duplicate.js
@@ -0,0 +1,42 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+*/ 
+
+const containDuplicate = (nums) => {
+  let map = {};
+  let count = 1;
+  for(let i = 0; i < nums.length; i++) {
+    
+  }
+}
+console.log(containDuplicate([1,2,3,1])); // true
+console.log(containDuplicate([1,1,1,3,3,4,3,2,4,2])); // true
+console.log(containDuplicate([1,2,3,4])); // false
