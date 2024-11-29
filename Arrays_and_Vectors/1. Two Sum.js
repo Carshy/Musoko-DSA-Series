4067edf6cd1e1485bd62facec932d258ce9a44a2
carshy <mussocollins@gmail.com>
2024-11-29 03:27:40 +0300

    Add map object

--- Arrays_and_Vectors/1. Two Sum.js
+++ Arrays_and_Vectors/1. Two Sum.js
@@ -19,15 +19,24 @@ Input: nums = [3,3], target = 6
 Output: [0,1]
 */
 
+// const twoSum = (nums, target) => {
+//   let map = {};
+//   for(let i = 0; i < nums.length; i++) {
+//     let diff = target - nums[i];
+//     if(map[diff] !== undefined) {
+//       return [diff, nums[i]]
+//     }
+//     map[nums[i]] = i;
+//   }
+// };
 const twoSum = (nums, target) => {
-  let map = {};
+  const map = {};
   for(let i = 0; i < nums.length; i++) {
     let diff = target - nums[i];
     if(map[diff] !== undefined) {
-      return [diff, nums[i]]
+      return [map[diff], i];
     }
-    map[nums[i]] = i;
   }
-};
+}
 console.log(twoSum([2,7,11,15], 9));
 console.log(twoSum([3,2,4], 7));
