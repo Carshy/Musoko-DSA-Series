66acaff1afba0e4aa962428bf8d5f8c05aed2419
carshy <mussocollins@gmail.com>
2024-12-02 06:20:57 +0300

    Add compression for cube

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,6 +1,6 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
 def my_list(lst):
-  sqrd = [x*9 for x in lst]
+  sqrd = [x**3 for x in lst]
   return sqrd
 print(my_list([3, 4, 2, 9, 7]))
