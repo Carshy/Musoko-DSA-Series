ff820beb659ecada5f4d8ac37f653d152fa84bc2
carshy <mussocollins@gmail.com>
2024-12-02 06:02:07 +0300

    Slice the list for reverse

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,6 +1,6 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
 def my_list(lst):
-  lst.reverse()
-  return lst
+  reversed = lst[::-1]
+  return reversed
 print(my_list(['banana', 3, 3.14, True, 'banana']))
