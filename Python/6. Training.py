fe1ba1429cdf082aa6c16543485eddb93bace626
carshy <mussocollins@gmail.com>
2024-12-02 06:20:02 +0300

    Add list comprehension

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,5 +1,6 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
 def my_list(lst):
-  return lst[::-1]
-print(my_list(['banana', 3, 3.14, True, 'banana']))
+  sqrd = [x*9 for x in lst]
+  return sqrd
+print(my_list([3, 4, 2, 9, 7]))
