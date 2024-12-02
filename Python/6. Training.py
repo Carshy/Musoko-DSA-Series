900c19b72d5f1bf25feb37b973ae1292768d0e05
carshy <mussocollins@gmail.com>
2024-12-02 05:54:44 +0300

    Add list manipulation

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,6 +1,6 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
 def my_list(lst):
-  lst
-  return lst[0].upper()
-print(my_list(['banana', 3, 3.14, True]))
+  timess = lst.count('banana')
+  return timess
+print(my_list(['banana', 3, 3.14, True, 'banana']))
