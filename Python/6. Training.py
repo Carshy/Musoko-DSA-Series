042e56ec285f244d8fdb14ed3dcefb2433113f2d
carshy <mussocollins@gmail.com>
2024-12-03 12:28:45 +0300

    Change to square

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,6 +1,6 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
 def my_list(lst):
-  sqrd = [x**3 for x in lst if x % 2 != 0]
+  sqrd = [x**2 for x in lst if x % 2 != 0]
   return sqrd
 print(my_list([3, 4, 2, 9, 7]))
