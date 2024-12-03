882f3e704f7c133aed9150ceac555f6471f8f9f7
carshy <mussocollins@gmail.com>
2024-12-03 12:45:17 +0300

    Add range

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,4 +1,8 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
 # get a list of numbers from 0 to 10
-def 
+def numList(nums):
+  numbers = []
+  for i in range(nums):
+    return numbers.append(i)
+print(numList([10]))
