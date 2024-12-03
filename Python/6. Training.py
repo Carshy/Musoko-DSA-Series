a48536041077ff6d9f64f2a7fe626b6e9375ad34
carshy <mussocollins@gmail.com>
2024-12-03 13:02:18 +0300

    Convert words to uppercase

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,9 +1,8 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
-# get a list of numbers from 0 to 10
-def numList(nums):
-  numbers = []
-  for i in range(nums):
-    numbers.append(i)
-  return numbers
-print(numList(10))
+# have a list of words, I want you to filter out the short words and then convert the longer words to uppercase
+
+def wordsCon(words):
+  results = [word.upper() for word in words if len(word) > 2 ]
+  return results
+print(wordsCon["apple", "banana", "mango", "is", "he", "person"])
