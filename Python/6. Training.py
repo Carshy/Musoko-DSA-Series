85b1f0b57a5e70254e99de73da86c17d38d81a9b
carshy <mussocollins@gmail.com>
2024-12-03 12:42:10 +0300

    Get number list from 0 to 10

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,6 +1,4 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
-def sqrList(lst):
-  sqrd = [x**2 for x in lst if x % 2 != 0]
-  return sqrd
-print(sqrList([3, 7, 4, 2, 10])) ## [9, 49]
+# get a list of numbers from 0 to 10
+def 
