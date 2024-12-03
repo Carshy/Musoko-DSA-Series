5b325ddedfef48087cac634b9261267ffffb0f02
carshy <mussocollins@gmail.com>
2024-12-03 12:34:50 +0300

    Implement List comprehension

--- Python/6. Training.py
+++ Python/6. Training.py
@@ -1,6 +1,6 @@
 # list in python is a data structure or rather a variable that is used to hold mutable data. what i mean is that it can hold different types of data for instance a string, a number, a float, or even a boolean
 
-def my_list(lst):
+def sqrList(lst):
   sqrd = [x**2 for x in lst if x % 2 != 0]
   return sqrd
-print(my_list([3, 4, 2, 9, 7]))
+print(sqrList([3, 7, 4, 2, 10])) ## [9, 49]
